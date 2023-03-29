227870ab384c137487f846450f4ccf0574150da0
sjunh812 <sjunh812@naver.com>
2023-03-29 20:16:14 +0900

    part2 ch6. 채팅 앱
    - import Firebase Messaging Library
    - 채팅 수신 기능 구현

---
+++ part2/chapter6/app/src/main/java/org/sjhstudio/fastcampus/part2/chapter6/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,60 @@
+package org.sjhstudio.fastcampus.part2.chapter6.firebase
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import org.sjhstudio.fastcampus.part2.chapter6.R
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    private val channelId = getString(R.string.notification_channel_id)
+    private val notificationManager: NotificationManager by lazy {
+        getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
+
+    companion object {
+        private const val LOG = "MyFirebaseMessagingService"
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.e(LOG, "onNewToken(): $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        createNotificationChannel()
+
+        message.notification?.let { messageNotification ->
+            if (messageNotification.title != null && messageNotification.body != null) {
+                createNotification(messageNotification.title!!, messageNotification.body!!)
+            }
+        }
+    }
+
+    private fun createNotificationChannel(): NotificationChannel {
+        val name = getString(R.string.notification_channel_name)
+        val description = getString(R.string.notification_channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val notificationChannel = NotificationChannel(channelId, name, importance)
+            .apply { setDescription(description) }
+
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        return notificationChannel
+    }
+
+    private fun createNotification(title: String, content: String) {
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.ic_chat_bubble_24)
+            .setContentTitle(title)
+            .setContentText(content)
+            .build()
+
+        notificationManager.notify(0, notification)
+    }
+}
