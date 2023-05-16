82dd54b146a28b07465eb0efbfb8729aeef08df6
sjunh812 <sjunh812@naver.com>
2023-05-16 17:59:01 +0900

    part2 ch10. 내일의집
    - Firebase 설정
    - 로그인 UI

--- part2/chapter10/app/src/main/java/org/sjhstudio/flow/chapter10/MainActivity.kt
+++ part2/chapter10/app/src/main/java/org/sjhstudio/flow/chapter10/MainActivity.kt
@@ -2,10 +2,23 @@ package org.sjhstudio.flow.chapter10
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.sjhstudio.flow.chapter10.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        with(binding) {
+
+        }
     }
 }
