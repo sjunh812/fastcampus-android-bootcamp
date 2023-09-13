8ff3b87bdcbb8426803b9c639c920030c3fd9c98
sjunh812 <sjunh812@gmail.com>
2023-09-13 23:31:26 +0900

    feat : part2. ch12 Youtube // exoplayer 연결
    - 영상 화면 MotionLayout 설정
    - exoPlayer 초기화 및 시작 로직 작성

--- part2/chapter12/app/src/main/java/org/sjhstudio/fastcampus/part2/chapter12/MainActivity.kt
+++ part2/chapter12/app/src/main/java/org/sjhstudio/fastcampus/part2/chapter12/MainActivity.kt
@@ -1,20 +1,32 @@
 package org.sjhstudio.fastcampus.part2.chapter12
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import org.sjhstudio.fastcampus.part2.chapter12.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var exoPlayer: ExoPlayer? = null
     private val videoListAdapter by lazy {
         VideoListAdapter {
+            // transition test
             binding.layoutMotion.setTransition(R.id.collapse, R.id.expand)
             binding.layoutMotion.transitionToEnd()
+
+            play(it)
         }
     }
     private var mockVideoList: VideoList? = null
+
+    override fun onStart() {
+        super.onStart()
+        initExoPlayer()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +40,44 @@ class MainActivity : AppCompatActivity() {
         binding.layoutMotion.jumpToState(R.id.collapse)
     }
 
+    override fun onResume() {
+        super.onResume()
+        initExoPlayer()
+    }
+
+    override fun onStop() {
+        exoPlayer?.pause()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        exoPlayer?.release()
+        super.onDestroy()
+    }
+
     private fun initViews() {
         with(binding) {
             rvVideoList.run {
                 layoutManager = LinearLayoutManager(this@MainActivity)
                 adapter = videoListAdapter
             }
+        }
+    }
+
+    private fun initExoPlayer() {
+        if (exoPlayer == null) {
+            exoPlayer = ExoPlayer.Builder(this).build()
+                .also {
+                    binding.videoPlayerView.player = it
+                }
+        }
+    }
+
+    private fun play(videoItem: VideoItem) {
+        exoPlayer?.run {
+            setMediaItem(MediaItem.fromUri(Uri.parse(videoItem.videoUrl)))
+            prepare()
+            play()
         }
     }
 
