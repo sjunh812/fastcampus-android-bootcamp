849fcb365e5d8b311ab5e5223857bb966376bbb9
sjunh812 <sjunh812@gmail.com>
2023-11-04 12:23:20 +0900

    feat: part3. ch2 금융 서비스(보안 키패드 / 휴대폰 인증) // 프로젝트 마무리
    - ContextCompat.registerReceiver() 오류 해결 (RECEIVER_EXPORTED)

--- part3/chapter2/app/src/main/java/com/example/chapter2/receiver/AuthOtpReceiver.kt
+++ part3/chapter2/app/src/main/java/com/example/chapter2/receiver/AuthOtpReceiver.kt
@@ -4,12 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.util.Log
 import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
 
 /**
- * 1. 문자 내용이 140 바이트를 초과하면 안된다.
+ * bf0pjx5qeUg
+ * 1. 문자 내용이 140 byte 를 초과하면 안된다.
  * 2. 문자 맨 앞에 <#> 가 포함이 되어야 한다.
  * 3. 맨 마지막에 앱을 식별하는 11글자 해시코드가 존재해야 한다.
  */
@@ -24,10 +26,12 @@ class AuthOtpReceiver : BroadcastReceiver() {
                 when (status.statusCode) {
                     CommonStatusCodes.SUCCESS -> {
                         val otpSms = bundle.getString(SmsRetriever.EXTRA_SMS_MESSAGE, "")
+                        Log.e("sjh", "onReceive :: otpSms >> $otpSms")
                         if (listener != null && otpSms.isNotEmpty()) {
                             val otp = PATTERN.toRegex().find(otpSms)?.destructured?.component1()
                             if (!otp.isNullOrBlank()) {
-                                listener?.onOtpReceived(otp)
+                                Log.e("sjh", "onReceive :: otp >> $otp")
+                                listener!!.onOtpReceived(otp)
                             }
                         }
                     }
@@ -49,6 +53,6 @@ class AuthOtpReceiver : BroadcastReceiver() {
     }
 
     companion object {
-        private const val PATTERN = "^<#>.*\\[Sample].+\\[(\\d{6})].+\$"
+        private const val PATTERN = "^<#>.*\\[Sample\\].+\\[(\\d{6})\\].+\$"
     }
 }
