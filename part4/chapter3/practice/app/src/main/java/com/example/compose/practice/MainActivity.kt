e684dc8007d8dac6eb80c98c1800a98145cab3df
sjunh812 <sjunh812@gmail.com>
2024-04-29 17:20:07 +0900

    feat: part4. ch4-8 ~ ch4-11 [Jetpack Compose]

--- part4/chapter3/practice/app/src/main/java/com/example/compose/practice/MainActivity.kt
+++ part4/chapter3/practice/app/src/main/java/com/example/compose/practice/MainActivity.kt
@@ -4,21 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.compose.practice.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,12 +39,13 @@ class MainActivity : ComponentActivity() {
             PracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                    DropdownMenuEx()
                 }
             }
         }
     }
 }
+
 @Composable
 fun CanvasEx() {
     Canvas(modifier = Modifier.size(30.dp)) {
@@ -72,10 +84,147 @@ fun CanvasEx() {
     }
 }
 
+@Composable
+fun DialogEx() {
+    var showDialog by remember { mutableStateOf(false) }
+    var count by remember { mutableStateOf(0) }
+
+    Column {
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Open dialog")
+        }
+        Text(text = "count : $count")
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            confirmButton = {
+                Button(onClick = {
+                    count++
+                    showDialog = false
+                }) {
+                    Text(text = "Add")
+                }
+            },
+            dismissButton = {
+                Button(onClick = {
+                    showDialog = false
+                }) {
+                    Text(text = "Cancel")
+                }
+            },
+            title = {
+                Text("Notification")
+            },
+            text = {
+                Text("When click the add button, then count is Added\nPlease click the button")
+            }
+        )
+    }
+}
+
+@Composable
+fun CustomDialogEx() {
+    var showDialog by remember { mutableStateOf(false) }
+    var count by remember { mutableStateOf(0) }
+
+    Column {
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Open dialog")
+        }
+        Text(text = "count : $count")
+    }
+
+    if (showDialog) {
+        Dialog(onDismissRequest = { showDialog = false }) {
+            Surface {
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "Title")
+                    Text(text = "Please click the buttons")
+                    ConstraintLayout {
+                        val (btnPlus, btnMinus, btnCancel) = createRefs()
+                        Button(
+                            onClick = { showDialog = false },
+                            modifier = Modifier.constrainAs(btnCancel) {
+                                end.linkTo(btnPlus.start, margin = 4.dp)
+                                centerVerticallyTo(btnPlus)
+                            }) {
+                            Text(text = "Cancel")
+                        }
+                        Button(
+                            onClick = {
+                                count++
+                                showDialog = false
+                            },
+                            modifier = Modifier.constrainAs(btnPlus) {
+                                end.linkTo(btnMinus.start, margin = 4.dp)
+                                centerVerticallyTo(btnMinus)
+                            }
+                        ) {
+                            Text(text = "+")
+                        }
+                        Button(
+                            onClick = {
+                                count--
+                                showDialog = false
+                            },
+                            modifier = Modifier.constrainAs(btnMinus) {
+                                end.linkTo(parent.end)
+                                bottom.linkTo(parent.bottom)
+                            }
+                        ) {
+                            Text(text = "-")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DropdownMenuEx() {
+    var expandDropdownMenu by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
+
+    Column {
+        Button(onClick = {
+            expandDropdownMenu = true
+        }) {
+            Text("Open DropdownMenu")
+        }
+        Text("Counter : $counter")
+    }
+
+    DropdownMenu(
+        expanded = expandDropdownMenu,
+        onDismissRequest = {
+            expandDropdownMenu = false
+        }
+    ) {
+        DropdownMenuItem(text = { Text("plus") }, onClick = { counter++ })
+        DropdownMenuItem(text = { Text("minus") }, onClick = { counter-- })
+    }
+}
+
+@Composable
+fun SnackbarEx() {
+    var counter by remember { mutableStateOf(0) }
+    val snackbarHostState = remember { SnackbarHostState() }
+//    val scaffoldState = rememberScaff
+//    Scaffold(snackbarHost = snackbarHostState) {
+//
+//    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PracticeTheme {
-        CanvasEx()
+//        CanvasEx()
+//        DialogEx()
+//        CustomDialogEx()
+        DropdownMenuEx()
     }
 }
