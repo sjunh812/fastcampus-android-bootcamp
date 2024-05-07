e1053f62dc615a6f900eeadfd6767c67af83dfb7
sjunh812 <sjunh812@gmail.com>
2024-05-07 20:44:45 +0900

    feat: part4. ch4-11 [Jetpack Compose]

--- part4/chapter3/practice/app/src/main/java/com/example/compose/practice/MainActivity.kt
+++ part4/chapter3/practice/app/src/main/java/com/example/compose/practice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.compose.practice
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,15 +13,20 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -31,6 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.compose.practice.ui.theme.PracticeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +46,8 @@ class MainActivity : ComponentActivity() {
             PracticeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    DropdownMenuEx()
+//                    DropdownMenuEx()
+                    SnackbarEx()
                 }
             }
         }
@@ -208,14 +216,34 @@ fun DropdownMenuEx() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SnackbarEx() {
     var counter by remember { mutableStateOf(0) }
     val snackbarHostState = remember { SnackbarHostState() }
-//    val scaffoldState = rememberScaff
-//    Scaffold(snackbarHost = snackbarHostState) {
-//
-//    }
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold {
+        Surface(modifier = Modifier.padding(it)) {
+            Button(onClick = {
+                counter++
+                coroutineScope.launch {
+                    val snackbarResult = snackbarHostState.showSnackbar(
+                        message = "카운터는 $counter 입니다.",
+                        actionLabel = "닫기",
+                        duration = SnackbarDuration.Short
+                    )
+                    when (snackbarResult) {
+                        SnackbarResult.ActionPerformed -> {}
+                        SnackbarResult.Dismissed -> {}
+                    }
+                }
+            }) {
+                Text("더하기")
+            }
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -225,6 +253,7 @@ fun GreetingPreview() {
 //        CanvasEx()
 //        DialogEx()
 //        CustomDialogEx()
-        DropdownMenuEx()
+//        DropdownMenuEx()
+        SnackbarEx()
     }
 }
