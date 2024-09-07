bedcd6a765d63f15505fe8f90e480eaac9e9e591
sjunh812 <sjunh812@gmail.com>
2024-09-07 15:56:52 +0900

    feat: part4. ch5-9 포켓몬 앱 // pokemon 상세 구현 중

--- part4/chatper5/Pokemon/app/src/main/java/com/sjhstudio/compose/pokemon/ui/screen/types/MainNavRoute.kt
+++ part4/chatper5/Pokemon/app/src/main/java/com/sjhstudio/compose/pokemon/ui/screen/types/MainNavRoute.kt
@@ -1,5 +1,12 @@
 package com.sjhstudio.compose.pokemon.ui.screen.types
 
 enum class MainNavRoute {
-    Home
+    Home,
+    Detail;
+
+    companion object {
+        fun getDetailRoute() = "${Detail.name}/{pokemonId}"
+
+        fun getDetailRealRoute(pokemonId: Int) = "${Detail.name}/$pokemonId"
+    }
 }
