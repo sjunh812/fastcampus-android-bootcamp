603fa051f95a5e117b7f9a1248d992aeef315597
sjunh812 <sjunh812@gmail.com>
2024-09-11 22:33:45 +0900

    feat: part4. ch5-9 포켓몬 앱 // 마무리

--- part4/chatper5/Pokemon/app/src/main/java/com/sjhstudio/compose/pokemon/ui/screen/DetailScreen.kt
+++ part4/chatper5/Pokemon/app/src/main/java/com/sjhstudio/compose/pokemon/ui/screen/DetailScreen.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,7 +33,9 @@ fun DetailScreen(
             .padding(8.dp)
             .fillMaxSize()
     ) {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             val pokemonResult = viewModel.pokemonResult
 
             AsyncImage(
@@ -43,6 +47,9 @@ fun DetailScreen(
                 text = pokemonResult.species.name,
                 fontSize = 18.sp
             )
+            Button(onClick = onUpButtonClick) {
+                Text(text = "Back")
+            }
         }
     }
 }
